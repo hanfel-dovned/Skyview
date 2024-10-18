606b566eca5f0161409c3fbaf5e9dc2615cb69fb
bonbud-macryg <101650933+bonbud-macryg@users.noreply.github.com>
2024-10-18 17:39:59 +0100

    PWA on iOS (#11)

---
+++ sky-view/service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "athens-cache-v1";
+const assetsToCache = [
+  "/",
+  "./feather.css",
+  "./vi-ew.js",
+  "./wi-nd.js",
+  "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+];
+
+// Install the service worker
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+// Intercept fetch requests
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate and clean up old caches
+self.addEventListener("activate", event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
